615ac247d9ca220083fce67abc6b44ab1794af20
sa77irak <sa77irak@gmail.com>
2019-09-18 12:18:01 +0900

    creating our own middleware

--- app.js
+++ app.js
@@ -5,16 +5,39 @@ const express = require('express');
 const app = express();
 
 // app.use to use middleware.
-// express.json() is middleware. middleware is basically just a function that can modify the incoming request data, a step that the request goes through while it's being processed.
+// express.json() is a middleware. express.json() returns a function. middleware is basically just a function that can modify the incoming request data, a step that the request goes through while it's being processed.
 app.use(express.json());
+
+// defining our own custom middleware.
+// this middleware here applys to each and every single request.because we didnt specify any route.
+// in express the ORDER of the middleware in the code matters. the request-response cycle is LINEAR.
+// middleware stack을 따라서 순서대로 시행 되기에, 초반부에 이렇게 넣으면 모든 request에 대해 적용되는 셈.
+app.use((req, res, next) => {
+  console.log('Hello from the middleware ^__^)/');
+  // we need to call the next function, because otherwise the request-response cycle will get stuck at this point.
+  next();
+});
+
+// another custom middleware
+// to manipulate the request object. to add the current time to the request.
+app.use((req, res, next) => {
+  // Let's pretend we have some route handler that really needs the info about when exactly the request happens.
+  // then we can define a property on the request object called requestTime and set it to a value representing the time that the request happened.
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+  // we can use the info from the middleware in this route handler for example.
+  console.log(req.requestTime);
+
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours
@@ -103,10 +126,12 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// our route handlers here are actually kind of middleware themselves. They are simply middleware functions that only apply for a certain URL so a certain route.
 app
   .route('/api/v1/tours')
   .get(getAllTours)
   .post(createTour);
+
 app
   .route('/api/v1/tours/:id')
   .get(getTour)
