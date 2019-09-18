6c52100a538c6874f27400566c45c94dc60177b9
sa77irak <sa77irak@gmail.com>
2019-09-18 23:42:54 +0900

    param middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,22 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+// using the concept of param middleware, we are going to perform this check here in a outside middleware that is going to run before the request even hits the handler functions. so this middleware is part of our pipline.
+// you might argue that we might simply create a simple function which could also check for the id and call that function inside each of the tourfunction..
+// but that goes against the philosophy of express, where we should always work withthe middleware stack, so with this pipeline as much as we can.
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+
+  if (req.params.id * 1 > tours.length) {
+    // we have to return it. because we want to make sure after sending this response the function will return. so that it'll finish and will never call the next();
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   // we can use the info from the middleware in this route handler for example.
   console.log(req.requestTime);
@@ -80,13 +96,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-
   res.status(204).json({
     status: 'success',
     data: null
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,7 +2,13 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 
 // If we want to separate the resources for routes and route handlers, we have to have one separate router for each of our resources.
+// Each router is kind of a mini sub application one for each resource.
 const router = express.Router();
+
+// param middleware : middleware that only runs for certain parameters. so when we have a certain parameter in our url.
+// since this middleware is only specified on this router, well then of course it is only part of the middleware stack if we are actually inside of this sub application.
+// using the concept of param middleware, we are going to perform this check here in a outside middleware that is going to run before the request even hits the handler functions.
+router.param('id', tourController.checkID);
 
 router
   .route('/')
