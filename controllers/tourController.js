502b73d1129ca4232adb61e9c83cec207a1203fb
sa77irak <sa77irak@gmail.com>
2019-09-21 14:19:05 +0900

    another way of creating documents, fixed eslint async issue by modifying package.json file

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,6 +29,8 @@ exports.checkID = (req, res, next, val) => {
 };
 */
 
+/*
+////////////NO LONGER NEEDED, BECAUSE MONGOOSE WILL GOING TO TAKE CARE OF IR////////////////
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
@@ -38,6 +40,7 @@ exports.checkBody = (req, res, next) => {
   }
   next();
 };
+*/
 
 exports.getAllTours = (req, res) => {
   // we can use the info from the middleware in this route handler for example.
@@ -67,34 +70,50 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  // we have to use middleware for the req to have the body property.
-  // console.log(req.body);
+exports.createTour = async (req, res) => {
+  try {
+    // we can do it like this to create documents.
+    // we create the tour from the model, and then use the save method on that tour. this save method returns a promise.
+    // const newTour = new Tour({});
+    // newTour.save();
 
-  res.status(201).json({
-    status: 'success'
-    // data: {
-    //   tour: newTour
-    // }
-  });
+    // but.. we can also do it like this to create a document.
+    // we call the create method right on the model itself. this create method also returns a promise. but we are going to use async/await this time.
+    const newTour = await Tour.create(req.body);
 
-  // const newId = tours[tours.length - 1].id + 1;
-  // const newTour = Object.assign({ id: newId }, req.body);
+    // we have to use middleware for the req to have the body property.
+    // console.log(req.body);
 
-  // tours.push(newTour);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
 
-  // fs.writeFile(
-  //   `${__dirname}/dev-data/data/tours-simple.json`,
-  //   JSON.stringify(tours),
-  //   err => {
-  //     res.status(201).json({
-  //       status: 'success',
-  //       data: {
-  //         tour: newTour
-  //       }
-  //     });
-  //   }
-  // );
+    // const newId = tours[tours.length - 1].id + 1;
+    // const newTour = Object.assign({ id: newId }, req.body);
+
+    // tours.push(newTour);
+
+    // fs.writeFile(
+    //   `${__dirname}/dev-data/data/tours-simple.json`,
+    //   JSON.stringify(tours),
+    //   err => {
+    //     res.status(201).json({
+    //       status: 'success',
+    //       data: {
+    //         tour: newTour
+    //       }
+    //     });
+    //   }
+    // );
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!'
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
