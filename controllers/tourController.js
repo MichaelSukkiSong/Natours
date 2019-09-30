9baec433e510d22692666ec18f7f9234e79083f3
sa77irak <sa77irak@gmail.com>
2019-09-30 21:54:59 +0900

    adding 404 not found errors

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,7 @@
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 /*
 //////////TESTING////////////
@@ -166,6 +167,10 @@ exports.getTour = catchAsync(async (req, res, next) => {
   // find(),findByIdAndUpdate(),findById() returns query objects, and later on can be used to immplement sorting/filtering.
   const tour = await Tour.findById(req.params.id);
 
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -235,6 +240,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     runValidators: true
   });
 
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -244,7 +253,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
 
   res.status(204).json({
     status: 'success',
