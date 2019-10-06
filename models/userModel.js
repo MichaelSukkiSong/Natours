e72c8fd2d54565c7b85ba2474751a032a5c21567
sa77irak <sa77irak@gmail.com>
2019-10-06 14:27:51 +0900

    logging in users-2

--- models/userModel.js
+++ models/userModel.js
@@ -47,6 +47,14 @@ userSchema.pre('save', async function(next) {
   next();
 });
 
+// instance method: basically a method that is going to be available on all documents of a certain collection
+userSchema.methods.correctPassword = async function(
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
