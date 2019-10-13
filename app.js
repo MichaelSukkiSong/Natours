a8349f80217287293861bda5794236e280065e2c
sa77irak <sa77irak@gmail.com>
2019-10-13 15:12:17 +0900

    data sanitization

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -51,6 +53,12 @@ app.use('/api', limiter);
 // express.json() is a middleware. express.json() returns a function. middleware is basically just a function that can modify the incoming request data, a step that the request goes through while it's being processed.
 // express does not put that body data on the req. so in order to have that data available we use this middleware.
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Serving static files
 // simple built in middleware to serve static files.
