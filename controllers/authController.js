c43faffa37dfcab7f47b2616901b25718a10f861
sa77irak <sa77irak@gmail.com>
2019-10-13 12:54:35 +0900

    sending jwt via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,21 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    // this makes it so that the cookie can not be accessed or modified in any way by the browser
+    // all the browser is going to do when we set httpOnly to true, is to basically receive the cookie, store it, and then send it automatically along with every request.
+    httpOnly: true
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  // defing the cookie, and sending it using res.cookie
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from output
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
